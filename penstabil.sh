972753525020d820fcbbbe0994372398e2b49a53
SantriTeknologi1 <56202312+SantriTeknologi1@users.noreply.github.com>
2019-10-10 15:58:34 +0700

    Add files via upload

---
+++ penstabil.sh
@@ -0,0 +1,62 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[37;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Santri
+echo $green"Welcome to All"
+echo
+sleep 2
+echo $yellow"Silahkan pilih Tools :"
+echo
+echo $yellow"1.) Stabilkan jaringan"
+echo
+echo $yellow"2.) install buat pemula"
+echo
+echo $red"0.) Keluar !!"
+echo
+echo $yellow
+read -p "Silahkan pilih:" bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay  By Santri
+echo $green"Stabilkan jaringan"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay lanjutkan
+echo $white"#pkg install update && upgrade"
+echo $whie"#pkg install python2"
+echo $white"#pkg install lolcat"
+echo $whie"#pkg install ruby"
+echo $whie"#pkg install figlet"
+echo $whie"#pkg install php"
+echo $whie"#pkg install bash"
+echo $whie"#pkg install curl"
+echo $whie"#pkg install git"
+echo $whie"#pkg install gem"
+echo $whie"#pip2 install requests"
+echo $whie"#pkg install toilet"
+echo $whie"#gem install lolcat"
+sleep 2
+fi
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "Bye All"
+echo $yellow"To be continued"
+sleep 3
+exit
+fi
